d46bc9a3020977aca5c08516d46196590c2fa9ea
Evan Paul <evanpaul22@gmail.com>
2017-10-12 00:49:29 -0700

    Add basic cluster logic

--- game.py
+++ game.py
@@ -1,4 +1,28 @@
 import numpy as np
+import copy
+
+
+class Cluster:
+    def __init__(self, value):
+        self.value = value
+        self.cells = []
+
+    def add_cell(self, y, x):
+        self.cells.append((y, x))
+
+    def display(self, grid):
+        if len(self.cells) == 1:
+            return
+        g = copy.deepcopy(grid)
+        for c in self.cells:
+            g[c[0]][c[1]] = "X"
+        print(g)
+
+
+class Neighbor:
+    def __init__(self, y, x):
+        self.y = y
+        self.x = x
 
 
 def read_input(fname):  # -> n, p, t, board
@@ -24,8 +48,6 @@ def read_input(fname):  # -> n, p, t, board
     assert(t > 0.0)
     assert(len(temp_grid) == n)
 
-    # REVIEW Would forcing asterisk to a magic integer give us computational
-    # bonus due to consistent data type? If so, is it even worth it?
     grid = []
     for row in temp_grid:
         items = []
@@ -35,6 +57,7 @@ def read_input(fname):  # -> n, p, t, board
     grid = np.array(grid)
 
     return n, p, t, grid
+
 
 def printg(grid):
     n = len(grid)
@@ -68,18 +91,71 @@ def coord_to_indices(coord_string):
     row = int(coord_string[2]) - 1
 
 
-def find_clusters(grid):
-    pass
+# REVIEW THIS NEEDS MORE THOROUGH TESTING
+def get_clusters(grid, checked):
+    clusters = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            # REVIEW Does scoring care about fruit type i.e. should we ignore 0?
+            if not checked[i][j] and grid[i][j] != "*":
+                new_cluster = find_cluster(grid, i, j, checked)
+                clusters.append(new_cluster)
+    return clusters
 
 
+def find_cluster(grid, y, x, checked):
+    target_value = grid[y][x]
+    c = Cluster(target_value)
+    checked[y][x] = True
+    c.add_cell(y, x)
+    neighbors = get_valid_neighbors(grid, target_value, y, x, checked)
+    # Go through every valid neighbor to find the entirety of the cluster
+    while neighbors:
+        current = neighbors.pop(0)
+        c.add_cell(current.y, current.x)
+        checked[current.y][current.x] = True
+        neighbors += get_valid_neighbors(
+            grid, target_value, current.y, current.x, checked)
+
+    return c
+
+
+def get_valid_neighbors(grid, target_value, y, x, checked):
+    neighbors = []
+    # Up
+    if y - 1 >= 0 and grid[y - 1][x] == target_value and not checked[y - 1][x]:
+        neighbors.append(Neighbor(y - 1, x))
+    # Down
+    if y + 1 < len(grid) and grid[y + 1][x] == target_value and not checked[y + 1][x]:
+        neighbors.append(Neighbor(y + 1, x))
+    # Left
+    if x - 1 >= 0 and grid[y][x - 1] == target_value and not checked[y][x - 1]:
+        neighbors.append(Neighbor(y, x - 1))
+    # Right
+    if x + 1 < len(grid) and grid[y][x + 1] == target_value and not checked[y][x + 1]:
+        neighbors.append(Neighbor(y, x + 1))
+
+    return neighbors
+
+# TODO Return board after selecting cluster i.e. removing cluster and applying gravity
 def select_cluster(grid, cluster):
     pass
 
 
+def init_checked_map(n):
+    return np.array([np.array([False for x in range(n)]) for y in range(n)])
+
+
 if __name__ == "__main__":
     IN_DIR = "tests/in/"
-    n, p, t, grid = read_input(IN_DIR + "input_3.txt")
+    n, p, t, grid = read_input(IN_DIR + "input_5.txt")
     printg(grid)
+    checked = init_checked_map(len(grid))
+    clusters = get_clusters(grid, checked)
+
+    for clust in clusters:
+        clust.display(grid)
+
 '''
 
 OUTPUT
